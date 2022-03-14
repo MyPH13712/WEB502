416bc4dfe64908f4bcc8d22e6916c2dd0e184c03
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-03-14 15:12:09 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import ShowInfo from './components/ShowInfo'
-import type {ProductType} from './types/product';
+import type { ProductType } from './types/product';
 
 function App() {
-const [info, setInfo] = useState<ProductType>({
-  name: "My",
-  age: 20
-})
+  const [info, setInfo] = useState<ProductType>({
+    name: "My",
+    age: 20
+  })
+
+const [count, setCount] = useState<number>(0)
 
   return (
     <div className="App">
-        <ShowInfo info={info}/>
+      Count: {count} <button onClick={() => setCount(count + 1)}>Click</button>
+      <ShowInfo info={info} />
     </div>
   )
 }
