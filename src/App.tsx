9d4b42791d16fe10c04ce8154dedda571a3c8332
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-03-14 14:15:06 +0700

    buoi3

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import ShowInfo from './ShowInfo'
 
 type TProduct = {
   id: number,
-  name: string,
+  name: string
 }
 
 function App() {
   const [count, setCount] = useState<number>(0);
-  const [products, setProducts] = useState<TProduct[]>([{id:1, name: "My"}])
+  const [products, setProducts] = useState<TProduct[]>([{id: 1, name: "Dat"}])
 
   return (
     <div className="App">
-      <ShowInfo name = "dat"/>
+        <ShowInfo name="Dat"/>
     </div>
   )
 }
--- src/ShowInfo.tsx
+++ src/ShowInfo.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const ShowInfo = () => {
+const ShowInfo: React.FC<{name: string}> = ({name}) => {
   return (
     <div>ShowInfo</div>
   )
 }
+
+export default ShowInfo
