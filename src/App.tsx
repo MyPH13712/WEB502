b0e8f0607a8be63186b7249b07422d43793c5641
Mydtph <79831056+MyPH13712@users.noreply.github.com>
2022-03-16 15:42:17 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios';
 import './App.css'
+import ShowInfo from './components/ShowInfo'
 import type { ProductType } from './types/product';
 import { list, remove } from './api/product';
+import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import WebsiteLayout from './pages/layouts/WebsiteLayout';
+import Home from './pages/Home';
+import Product from './pages/Product';
+import AdminLayout from './pages/layouts/AdminLayout';
+import Dashboard from './pages/Dashboard';
+import ManagerProduct from './pages/ManagerProduct';
 function App() {
   const [products, setProducts] = useState<ProductType[]>([]);
   // const [count, setCount] = useState<number>(0);
-
   useEffect(() => {
     const getProducts = async () => {
       const { data } = await list();
@@ -14,32 +21,57 @@ function App() {
     }
     getProducts();
   }, [])
+
   const removeItem = async (id: number) => {
     // xoa tren API
     const { data } = await remove(id);
     // reRender
-    data && setProducts(products.filter(item => item._id !== data.id));
+    data && setProducts(products.filter(item => item._id !== data._id));
   }
   return (
     <div className="App">
-      <table>
+      {/* <table>
         <thead>
           <th>#</th>
           <th>Name</th>
           <th></th>
         </thead>
         <tbody>
-          {products.map((item, index) => {
+          {products && products.map((item, index) => {
             return <tr>
-              <td>{index + 1}</td>
-              <td>{item.name}</td>
-              <td>
-                <button onClick={() => removeItem(item._id)}>Remove</button>
-              </td>
-            </tr>
+                    <td>{index + 1}</td>
+                    <td>{item.name}</td>
+                    <td>
+                      <button onClick={() => removeItem(item._id)}>Remove</button>
+                    </td>
+                  </tr>
           })}
         </tbody>
-      </table>
+      </table> */}
+      <header>
+        <ul>
+          <li><NavLink to="/">Home Page</NavLink></li>
+          <li><NavLink to="/product">Product Page</NavLink></li>
+          <li><NavLink to="/admin/dashboard">Admin Dashboard</NavLink></li>
+        </ul>
+      </header>
+      <main>
+        <Routes>
+          {/* <Route path="/" element= {<h1>Home </h1>} />
+          <Route path="product" element= {<h1>Product</h1>} />
+          <Route path="about" element = {<h1>About </h1>}/> */}
+          <Route path="/" element={<WebsiteLayout />} >
+            <Route index element={<Home />} />
+            <Route path="product" element={<Product />} />
+          </Route>
+          <Route path="admin" element={<AdminLayout />}>
+            <Route index element={<Navigate to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="product" element={<ManagerProduct />} />
+          </Route>
+        </Routes>
+
+      </main>
     </div>
   )
 }
